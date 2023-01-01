dd592dcf86067abbded5cea508c2280de05479c4
Pavel Prokopenya <pavel12minsk@mail.ru>
2023-01-01 19:44:20 +0300

    feat: products can be added to cart

--- Online-store/src/pages/main/js/script.js
+++ Online-store/src/pages/main/js/script.js
@@ -1,5 +1,6 @@
 //import './modules/module';
 
+let db; // Use this array of objects to work with products database
 let productList = document.getElementById('products-list');
 
 function generateCart(iterator) {
@@ -20,7 +21,7 @@ function generateCart(iterator) {
 			</ul>
 		</div>
     <div class="item-buttons">
-      <button class="item-buttons__add">Add to cart</button>
+      <button class="item-buttons__add" id=product-num-${iterator.id}>Add to cart</button>
       <form action="../../pages/about/index.html">
         <button class="item-buttons__details">Details</button>
       </form>
@@ -36,6 +37,47 @@ async function getAllProducts() {
   for (let key in responceContentSliced) {
     await generateCart(responceContentSliced[key]);
   }
+  return responceContentSliced;
 }
 
-getAllProducts();
+// Entrance
+(() => {
+  if (!localStorage.getItem('RS-online-cart')) {
+    localStorage.setItem('RS-online-cart', JSON.stringify([]));
+  }
+
+  getAllProducts().then((array) => {
+    // console.log(JSON.parse(JSON.stringify(array)));
+    db = JSON.parse(JSON.stringify(array));
+    // console.log(db);
+    // return db;
+  });
+})();
+
+// TODO: в корзине выводить добавленные продукты.
+// TODO: в корзине изменять кол-во товаров + фиксировать в local storage.
+// TODO: в корзине реализовать удаление товаров.
+// TODO: пересчитывать на всех страницах сумму товаров в хедере.
+
+// TODO: передавать ID'шник по кнопке details и генерировать описание товара
+
+// Cart (add to cart button)
+productList.addEventListener('click', (e) => {
+  if (e.target.className === 'item-buttons__add') {
+    console.log(`${e.target.id}`);
+    const pickedProduct = db.find((x) => x.id === +e.target.id.split('-')[2]);
+    const objectToAdd = {
+      id: pickedProduct.id,
+      price: pickedProduct.price,
+      count: 1,
+    };
+    const cartArray = JSON.parse(localStorage.getItem('RS-online-cart'));
+    cartArray.push(objectToAdd);
+    localStorage.setItem('RS-online-cart', JSON.stringify(cartArray));
+
+    // TODO: не обращаться к серверу на каждое добавление товара
+    // TODO: добавлять в корзину, менять стили и текст кнопки.
+    // TODO: Кнопка меняется на удаление, после нажатия на удаление -
+    // удалять из локального хранилища по id'шнику, удалять стиль к дефолтному и менять текст
+  }
+});
