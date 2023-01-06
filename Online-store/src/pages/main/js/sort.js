febe6f461667fa4740d63d3387dcf1c209396fce
Oleg Ostrovskiy <remorselex99@gmail.com>
2023-01-06 20:57:31 +0300

    feat: partially implemented page-sorting logick

---
+++ Online-store/src/pages/main/js/sort.js
@@ -0,0 +1,20 @@
+const ascSort = document.getElementById('products__droplist-asc');
+const descSort = document.getElementById('products__droplist-desc');
+const select = document.getElementById('products__droplist');
+
+function sortByPrice(db) {
+  //db.sort((a,b) => a.price > b.price? 1: -1);
+  select.addEventListener('change', function () {
+    let optionText = this.options[this.selectedIndex].text;
+    if (optionText === ascSort.text) {
+      console.log(db);
+      return db.sort((a, b) => (a.price > b.price ? 1 : -1));
+    }
+    if (optionText === descSort.text) {
+      console.log(db);
+      return db.sort((a, b) => (a.price < b.price ? 1 : -1));
+    }
+  });
+}
+
+export default sortByPrice;
